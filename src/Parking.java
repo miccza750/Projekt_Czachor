8495f2fbeb5cb22200494bb139c6deff7503c65d
miccza750 <misiuczachor@o2.pl>
2025-05-27 15:52:02 +0200

    Dodanie plików

---
+++ src/Parking.java
@@ -0,0 +1,12 @@
+public class Parking {
+protected int ilosc_miejsc;
+protected boolean calo_dobowy;
+protected String godziny_otwarcia;
+
+    public Parking(int ilosc_miejsc, boolean calo_dobowy, String godziny_otwarcia) {
+        this.ilosc_miejsc = ilosc_miejsc;
+        this.calo_dobowy = calo_dobowy;
+        this.godziny_otwarcia = godziny_otwarcia;
+    }
+
+}
